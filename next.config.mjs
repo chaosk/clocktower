00000240fede9fd05adbd1e2f2c568dff9dab06d
Krzysztof Socha <ksocha@ksocha.com>
2025-10-27 18:08:10 +0100

    Add a way to join

--- next.config.mjs
+++ next.config.mjs
@@ -2,4 +2,14 @@ import nextra from 'nextra'
  
 const withNextra = nextra({})
  
-export default withNextra({})
+export default withNextra({
+  async redirects() {
+    return [
+      {
+        source: '/join-the-cult',
+        destination: process.env.DISCORD_URL,
+        permanent: false,
+      },
+    ];
+  },
+});
